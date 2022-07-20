638bc6fbe2675f4e4909ffecfc82f0685c801db3
ualiu <urim.aliu@gmail.com>
2022-07-20 11:36:37 -0400

    updated text

--- js/main.js
+++ js/main.js
@@ -8,14 +8,14 @@ const holdTime = totalTime / 5
 breatheAnimation()
 
 function breatheAnimation () {
-  text.innerHTML = 'Breath in for 3 seconds!'
+  text.innerHTML = 'Breath in!'
   container.className = 'container grow'
 
   setTimeout(() => {
-      text.innerText = 'Hold! for 1.5 second!'
+      text.innerText = 'Hold!'
 
       setTimeout(() => {
-          text.innerText = 'Breathe out! for 3 second!'
+          text.innerText = 'Breathe out!'
           container.className = 'container shrink'
       }, holdTime)
   }, breathTime)
